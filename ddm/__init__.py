3cbaf6da48637286572d2089a46e31165e6be58f
donadef <donat.defrancquen@gmail.com>
2018-09-07 18:31:55 +0200

    init back to what is was

--- ddm/__init__.py
+++ ddm/__init__.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+"""Top-level package for Double Decoupling Method (DDM)."""
+
+__author__ = """Dona de Francquen"""
+__email__ = 'donat.defrancquen@gmail.com'
+__version__ = '0.1.0'
